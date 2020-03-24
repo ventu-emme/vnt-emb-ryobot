f1c82e5d2fec7e6901b28ca5c41d4162f1b4a156
ventu-emme <ventu.emme@gmail.com>
2020-03-24 15:59:07 -0700

    Configured ROS workspace.

---
+++ ros_test_workspace/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vnt/robotics_corp/vnt-emb-ryobot/ros_test_workspace/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/vnt/robotics_corp/vnt-emb-ryobot/ros_test_workspace/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/vnt/robotics_corp/vnt-emb-ryobot/ros_test_workspace/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/vnt/robotics_corp/vnt-emb-ryobot/ros_test_workspace/src:$ROS_PACKAGE_PATH"
